3f17022887fc12b4fc747a5e5ed66be252a580b9
chae-hwa <ihj0519@naver.com>
2022-09-27 11:58:38 +0900

    [상품 옵션] 각 상품당 4개씩, 샘플 상품 옵션 8개 생성

--- src/main/java/com/ll/exam/spring_batch/app/product/service/ProductService.java
+++ src/main/java/com/ll/exam/spring_batch/app/product/service/ProductService.java
@@ -1,24 +1,31 @@
 package com.ll.exam.spring_batch.app.product.service;
 
 import com.ll.exam.spring_batch.app.product.entity.Product;
+import com.ll.exam.spring_batch.app.product.entity.ProductOption;
 import com.ll.exam.spring_batch.app.product.repository.ProductRepository;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.apache.catalina.Store;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class ProductService {
     private final ProductRepository productRepository;
 
-    public void create(String name , int price, String makerShopName) {
+    public void create(String name , int price, String makerShopName, List<ProductOption> options) {
 
         Product product = Product.builder()
                 .name(name)
                 .price(price)
                 .makerShopName(makerShopName)
                 .build();
+
+        for(ProductOption option : options ){
+            product.addOption(option);
+        }
 
         productRepository.save(product);
 
