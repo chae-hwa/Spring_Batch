0e1b229424f6f0c7cc24b03f80a5d3218d08465d
chae-hwa <ihj0519@naver.com>
2022-09-27 09:37:38 +0900

    [배치] 강제로 매번 다르게 실행하도록

--- src/main/java/com/ll/exam/spring_batch/job/helloWorld/HelloWorldJobConfig.java
+++ src/main/java/com/ll/exam/spring_batch/job/helloWorld/HelloWorldJobConfig.java
@@ -5,6 +5,7 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
@@ -20,6 +21,7 @@ public class HelloWorldJobConfig {
     @Bean
     public Job helloWorldJob(){ //Job 이름은 무조건 해야 된다. (딱 1개)
         return jobBuilderFactory.get("helloWorldJob")
+                .incrementer(new RunIdIncrementer()) // 실행마다 강제로 매번 다른 ID를 파라미터로 부여
                 .start(helloWorldStep1())
                 .build();
     }
