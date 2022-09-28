c3f34bf6ebb4c861fdad31a0c064e137a8569334
chae-hwa <ihj0519@naver.com>
2022-09-28 10:38:16 +0900

    [주문] 정산을 위한 주문 당시 가격 저장

--- src/main/java/com/ll/exam/spring_batch/app/order/entity/OrderItem.java
+++ src/main/java/com/ll/exam/spring_batch/app/order/entity/OrderItem.java
@@ -28,9 +28,16 @@ public class OrderItem extends BaseEntity {
 
     private int quantity;
 
+    // 후에 정산을 위해 가격 저장
+    private int price; // 권장판매가(판매)
+    private int salePrice; // 실제판매가(할인)
+    private int wholesalePrice; // 도매가(구매)
     public OrderItem(ProductOption productOption, int quantity){
         this.productOption = productOption;
         this.quantity=quantity;
+        this.price = productOption.getPrice();
+        this.price = productOption.getSalePrice();
+        this.price = productOption.getWholesalePrice();
     }
 
 }
