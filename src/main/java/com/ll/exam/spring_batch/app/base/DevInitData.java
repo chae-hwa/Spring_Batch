039efb43218cdb8b0eafb13f035893d9880e6f7f
chae-hwa <ihj0519@naver.com>
2022-09-28 21:44:01 +0900

    [캐시] 고객2가 주문2를 캐시 충전 후 결제

--- src/main/java/com/ll/exam/spring_batch/app/base/DevInitData.java
+++ src/main/java/com/ll/exam/spring_batch/app/base/DevInitData.java
@@ -44,17 +44,32 @@ public class DevInitData {
             Product product1 = productService.create("단가라 OPS", 68000, 45000, "청평화 A-1-15", Arrays.asList(new ProductOption("RED", "44"), new ProductOption("RED", "55"), new ProductOption("BLUE", "44"), new ProductOption("BLUE", "55")));
             Product product2 = productService.create("쉬폰 OPS", 72000, 55000,"청평화 A-1-15", Arrays.asList(new ProductOption("BLACK", "44"), new ProductOption("BLACK", "55"), new ProductOption("WHITE", "44"), new ProductOption("WHITE", "55")));
 
-            ProductOption productOption__RED__44 = product1.getProductOptions().get(0);
-            ProductOption productOption__BLUE__44 = product1.getProductOptions().get(2);
+            ProductOption product1Option__RED_44  = product1.getProductOptions().get(0);
+            ProductOption product1Option__BLUE_44  = product1.getProductOptions().get(2);
 
-            cartService.addItem(member1, productOption__RED__44, 1);
-            cartService.addItem(member1, productOption__RED__44, 2);
-            cartService.addItem(member1, productOption__BLUE__44, 1);
+            cartService.addItem(member1, product1Option__RED_44, 1);
+            cartService.addItem(member1, product1Option__RED_44, 2);
+            cartService.addItem(member1, product1Option__BLUE_44 , 1);
 
             Order order1 = orderService.createFromCart(member1); // member1의 장바구니에서 얻어오겠다.
 
             int order1PayPrice = order1.calculatePayPrice(); // 주문1의 계산 금액
             orderService.payByRestCashOnly(order1); // 캐시 결제
+
+            /* 고객2의 주문2 */
+            ProductOption product2Option__BLACK_44 = product2.getProductOptions().get(0);
+            ProductOption product2Option__WHITE_44 = product2.getProductOptions().get(2);
+
+            cartService.addItem(member2, product1Option__RED_44, 1); // 고객2 장바구니에 상품 담기
+            cartService.addItem(member2,product2Option__BLACK_44, 2);
+            cartService.addItem(member2,product2Option__WHITE_44, 2);
+
+            Order order2 = orderService.createFromCart(member2); //고객2의 장바구니 상품 가져오기
+
+            memberService.addCash(member2, 370_000,"충전_무통장입금");
+
+            orderService.payByRestCashOnly(order2); // 주문2 캐시로 결제하기
+
         };
     }
 }
