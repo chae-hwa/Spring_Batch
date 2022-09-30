a9a3d6bf692c4d7e96f94ca03a38b68fd74491ae
chae-hwa <ihj0519@naver.com>
2022-10-01 02:03:22 +0900

    [배치] Page로 범위 정산2 (Job에 의해 설정되도록)

--- src/main/java/com/ll/exam/spring_batch/app/order/repository/OrderItemRepository.java
+++ src/main/java/com/ll/exam/spring_batch/app/order/repository/OrderItemRepository.java
@@ -2,7 +2,18 @@ package com.ll.exam.spring_batch.app.order.repository;
 
 import com.ll.exam.spring_batch.app.order.entity.Order;
 import com.ll.exam.spring_batch.app.order.entity.OrderItem;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+
+    // 1 ~ n 개와 같이 범위(chunk)를 가져올 땐 list가 아닌 pageble로 불러와야 된다. (몇 억개가 될 수도)
+    // 그래서 List가 아닌 Page를 사용한 것.
+    Page<OrderItem> findAllByIdLessThan(long id, Pageable pageable);
+    // -> 실행쿼리 : SELECT * FROM order_item AS OI WHERE OI.id < 6;
+
+    // Between : from ~ to 까지 조회
+    Page<OrderItem> findAllByIdBetween(long fromId, long toId, Pageable pageable);
+    // -> 실행쿼리 : SELECT * FROM order_item AS OI WHERE OI.id BETWEEN 2 AND 7;
 }
