57a1bf623b41ef4b30bcf9202c3a273c6b861d50
chae-hwa <ihj0519@naver.com>
2022-10-01 21:59:51 +0900

    정산범위를 입력받은 연월에 결제된 건 수행

--- src/main/java/com/ll/exam/spring_batch/app/order/repository/OrderItemRepository.java
+++ src/main/java/com/ll/exam/spring_batch/app/order/repository/OrderItemRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
 
     // 1 ~ n 개와 같이 범위(chunk)를 가져올 땐 list가 아닌 pageble로 불러와야 된다. (몇 억개가 될 수도)
@@ -18,4 +20,6 @@ public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
     // -> 실행쿼리 : SELECT * FROM order_item AS OI WHERE OI.id BETWEEN 2 AND 7;
 
     Page<OrderItem> findAllByIsPaid (boolean isPaid, Pageable pageable);
+
+    Page<OrderItem> findAllByPayDateBetween(LocalDateTime fromDate, LocalDateTime toDate, Pageable pageable);
 }
